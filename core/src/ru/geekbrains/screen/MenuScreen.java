6b471bccd7af266e6a14e747dbe94be2256e9379
Daniil Gavrilov <dgavrilovdv@gmail.com>
2018-10-27 01:09:35 +0300

    from lesson2

---
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.Base2DScreen;
+
+public class MenuScreen extends Base2DScreen {
+
+    private SpriteBatch batch;
+    private Texture img;
+
+    private Vector2 pos;
+    private Vector2 v;
+
+
+    @Override
+    public void show() {
+        super.show();
+        batch = new SpriteBatch();
+        img = new Texture("star.jpg");
+        pos = new Vector2(0,0);
+        v = new Vector2(0.5f,0.3f); //скорость и направление смещения
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //очистка экрана
+        batch.begin();
+        batch.draw(img, pos.x, pos.y);
+        batch.end();
+        pos.add(v);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return super.touchDown(screenX, screenY, pointer, button);
+    }
+}
--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package ru.geekbrains.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.geekbrains.StarGame;
+
+import ru.geekbrains.Star2DGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StarGame(), config);
+		new LwjglApplication(new Star2DGame(), config);
 	}
 }
